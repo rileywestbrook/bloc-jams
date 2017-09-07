b8625d970a2b0b518a41768f3a38540d137cf71c
Riley Westbrook <westbrookriley@gmail.com>
2017-09-07 14:16:03 -0400

    Fixed code after watching assignment 8 solution video

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,12 +1,16 @@
-var revealPoint = function() {
+var animatePoints = function() {
 
    var points = document.getElementsByClassName('point');
 
+   var revealPoint = function(index) {
+     points[index].style.opacity = 1;
+     points[index].style.transform = "scaleX(1) translateY(0)";
+     points[index].style.msTransform = "scaleX(1) translateY(0)";
+     points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+   }
+
    for (var i = 0; i < points.length; i++) {
-     points[i].style.opacity = 1;
-     points[i].style.transform = "scaleX(1) translateY(0)";
-     points[i].style.msTransform = "scaleX(1) translateY(0)";
-     points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-   } 
+     revealPoint(i);
+   }
 
 };
