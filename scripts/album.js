b07170da4f3653383c7e2193bd667064c83de5b4
Riley Westbrook <westbrookriley@gmail.com>
2017-09-13 22:04:43 -0400

    assignment 13 done

--- scripts/album.js
+++ scripts/album.js
@@ -79,7 +79,12 @@ var findParentByClassName = function(element, targetClass) {
     var currentParent = element.parentElement;
     while (currentParent.className !== targetClass && currentParent.className !== null) {
       currentParent = currentParent.parentElement;
+    if (currentParent.className !== targetClass) {
+      console.log('No parent found.');
+    } else if (currentParent.className == null)
+    console.log('No parent found with that class name.');
   }
+
     return currentParent;
  }
 };
